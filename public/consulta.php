1a012d9422f4d318a8a060963811dd44e74c2172
julian777 <julian01cs@hotmail.com>
2016-08-26 09:50:00 -0600

    select

--- public/consulta.php
+++ public/consulta.php
@@ -1,5 +1,4 @@
 
-
 <html>
 
     <head>
@@ -51,65 +50,52 @@
                 </div>
             </div>
         </nav>
-        <?php
-        if (isset($_POST['update'])) {
-            $dbhost = 'localhost';
-            $dbuser = 'root';
-            $dbpass = 'root';
-            $dbname = "test_db";
 
-            $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+        </br>
+        </br>
+        </br>
+        </br>
 
-            if (!$conn) {
-                die('Could not connect: ' . mysql_error());
-            }
+        </br>
+        </br>
+        </br>
+        </br>
 
-            $emp_id = $_POST['emp_id'];
-            $emp_salary = $_POST['emp_salary'];
+        </br>
+        </br>
+        </br>
+        </br>
+    <center><p>
 
-            $sql = "UPDATE employee " . "SET emp_salary = $emp_salary " .
-                    "WHERE emp_id = $emp_id";
-            //  mysql_select_db('test_db');
-            $retval = $conn->query($sql);
-            //$retval = mysql_query( $sql, $conn );
-
-            if (!$retval) {
-                die('Could not update data: ' . mysql_error());
-            }
-            echo "Updated data successfully\n";
-            $conn->close();
-            // mysql_close($conn);
-            header("Location: http://localhost/aplicaciones/public/consulta.php");
-            die();
-        } else {
-            ?>
-            </br>
-            </br>
-            </br>
-            </br>
-            </br>
-
-            <form method = "post" action = "<?php $_PHP_SELF ?>">
-
-                <div class="form-group" >
-                    <label for="emp_id">ID:</label>
-                    <input type="text" class="form-control" name="emp_id"  id = "emp_id" />
-                </div>
-
-                <div class="form-group" >
-                    <label for="emp_id">Cantidad BTC:</label>
-                    <input type="text" class="form-control" name="emp_salary"  id = "emp_salary"/>
-                </div>
-
-                <div>
-                    <button type="submit" name="update" id="update" value="update" class="btn btn-primary">Registrarme</button>
-                </div>
-            </form>
             <?php
-        }
-        ?>
+            $servername = "localhost";
+            $username = "root";
+            $password = "root";
+            $dbname = "laravel";
+
+// Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT name, usd, btc FROM users";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while ($row = $result->fetch_assoc()) {
+                    echo "Cliente: " . $row["name"] . "</br> Saldo en USD: " . $row["usd"] . "</br> Saldo en BTC: " . $row["btc"] . "<br>";
+                }
+            } else {
+                echo "0 results";
+            }
+            $conn->close();
+            ?>
+
+        </p></center> 
 
 
-
-    </body>
+</body>
 </html>
